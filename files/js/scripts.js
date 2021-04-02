2dcb03d07a6e3659f91c540ec2e1198d131fd620
skipstorm <gcossu@gmail.com>
2021-04-02 04:23:52 +0200

    update scripts.js

--- files/js/scripts.js
+++ files/js/scripts.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
-var file = document.getElementById("fileForUpload").files[0];
-if (file) {
-    var reader = new FileReader();
-    reader.readAsText(file, "UTF-8");
-    reader.onload = function (evt) {
-        document.getElementById("fileContents").innerHTML = evt.target.result;
-    }
-    reader.onerror = function (evt) {
-        document.getElementById("fileContents").innerHTML = "error reading file";
-    }
+function onFileLoad(elementId, event) {
+    document.getElementById(elementId).innerText = event.target.result;
+}
+
+function onChooseFile(event, onLoadFileHandler) {
+    if (typeof window.FileReader !== 'function')
+        throw ("The file API isn't supported on this browser.");
+    let input = event.target;
+    if (!input)
+        throw ("The browser does not properly implement the event object");
+    if (!input.files)
+        throw ("This browser does not support the `files` property of the file input.");
+    if (!input.files[0])
+        return undefined;
+    let file = input.files[0];
+    let fr = new FileReader();
+    fr.onload = onLoadFileHandler;
+    fr.readAsText(file);
 }
   
 });
