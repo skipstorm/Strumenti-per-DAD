1bc9bd26c6fcb9c5954da4e478bddeaeee9421e4
Gianni Cossu <g.cossu@3logic.it>
2021-04-02 17:43:36 +0200

    lista partecipanti

--- files/js/scripts.js
+++ files/js/scripts.js
@@ -1,4 +1,31 @@
-//documentnt).ready(function(){
+$(document).ready(function(){
+    $(document).ready(function() {
+        $('.datatables').DataTable();
+    } );
+    $("[data-script-textfile]").each(function(){
+        var that = this;
+        $('input[type="file"]', that).on("change", function(event){
+            $(that).addClass('loading');
+            if (typeof window.FileReader !== 'function')
+                throw ("The file API isn't supported on this browser.");
+            let input = event.target;
+            if (!input)
+                throw ("The browser does not properly implement the event object");
+            if (!input.files)
+                throw ("This browser does not support the `files` property of the file input.");
+            if (!input.files[0])
+                return undefined;
+            let file = input.files[0];
+            let fr = new FileReader();
+            fr.onload = function(event){
+                $('textarea', that).text(event.target.result);
+                $(that).trigger("textfile:loaded");
+                $(that).removeClass('loading');
+            };
+            fr.readAsText(file);
+        });
+    });
+
 function onFileLoad(elementId, event) {
     document.getElementById(elementId).innerText = event.target.result;
 }
@@ -19,4 +46,4 @@ function onChooseFile(event, onLoadFileHandler) {
     fr.readAsText(file);
 }
   
-//});
+});
