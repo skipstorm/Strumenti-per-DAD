0fb25941bda0a17b472ea886a9ad842a2c96f9d3
skipstorm <gcossu@gmail.com>
2021-04-02 04:21:14 +0200

    update scripts.js

--- files/js/scripts.js
+++ files/js/scripts.js
@@ -1,1 +1,14 @@
-
+$(document).ready(function(){
+var file = document.getElementById("fileForUpload").files[0];
+if (file) {
+    var reader = new FileReader();
+    reader.readAsText(file, "UTF-8");
+    reader.onload = function (evt) {
+        document.getElementById("fileContents").innerHTML = evt.target.result;
+    }
+    reader.onerror = function (evt) {
+        document.getElementById("fileContents").innerHTML = "error reading file";
+    }
+}
+  
+});
